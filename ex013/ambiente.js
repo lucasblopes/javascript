d0e11e03c8f7607075f8673d844642b47476ea15
lucasblopes <lucaslopes900@hotmail.com>
2022-03-15 15:03:58 -0300

    15/03/22

---
+++ ex013/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <=6) {
+    console.log("Teste")
+    c++
+}
+*/
+
+var c = 1
+do {
+    console.log("Teste")
+    c++
+} while (c <= 6)
---
+++ ex014/ambiente.js
@@ -0,0 +1,10 @@
+/*
+var c = 1
+while (c <= 10) {
+    console.log(c)
+    c++
+} */
+
+for (var c=1; c<=10; c++) {
+    console.log(c)
+}
